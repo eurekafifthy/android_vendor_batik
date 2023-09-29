SuperiorOS/android_vendor_superior

config: permission: Add permissions
* cough * someone forgot to update permissions whitelist again.

Change-Id: Iedcdb0568ea1b9924144841a8d20ca6f05bbe29b

config/common.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
     vendor/superior/config/permissions/privapp-permissions-recorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-recorder.xml \
     vendor/superior/config/permissions/org.pixelexperience.recorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.pixelexperience.recorder.xml
 
+#Superior Permissions
+PRODUCT_COPY_FILES += \
+    vendor/superior/config/permissions/privapp-permissions-superior-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-superior.xml \
+
 # Include LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/superior/overlay/dictionaries
 

eurekafifthy/android_vendor_batik

Signed-off-by: eurekafifthy <bangmeyseries@gmail.com>

config/common.mk
@@ -56,6 +56,10 @@ PRODUCT_COPY_FILES += \
     vendor/batik/config/permissions/privapp-permissions-recorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-recorder.xml \
     vendor/batik/config/permissions/org.pixelexperience.recorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.pixelexperience.recorder.xml
 
+#Superior Permissions
+PRODUCT_COPY_FILES += \
+    vendor/batik/config/permissions/privapp-permissions-batik-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-batik.xml \
+
 # Include LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/batik/overlay/dictionaries
 